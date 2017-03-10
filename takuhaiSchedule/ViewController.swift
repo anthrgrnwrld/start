645620fb83896ea5d687f93abe78c2c36c32b6c7
anthrgrnwrld <anthrgrnwrld@gmail.com>
2017-03-10 19:28:07 +0900

    add realm and admob library

--- takuhaiSchedule/ViewController.swift
+++ takuhaiSchedule/ViewController.swift
@@ -7,17 +7,55 @@
 //
 
 import UIKit
+import RealmSwift
+import GoogleMobileAds
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, GADBannerViewDelegate, GADInterstitialDelegate {
+    
+    //    let YOUR_ID = "ca-app-pub-3530000000000000/0123456789"  // Enter Ad's ID here
+    let YOUR_BARNER_ID = "ca-app-pub-3530000000000000/0123456789"  // Enter Ad's ID here
+    let YOUR_INTERSTITIAL_ID = "ca-app-pub-3530000000000000/0123456789"  // Enter Ad's ID here
+    let TEST_DEVICE_ID = "4b4c24d168acf717000000000" // Enter Test ID here
+    let AdMobTest:Bool = true
+    let SimulatorTest:Bool = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Display Admob Barner
+        let bannerView:GADBannerView = getAdBannerView()
+        self.view.addSubview(bannerView)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    fileprivate func getAdBannerView() -> GADBannerView {
+        var bannerView: GADBannerView = GADBannerView()
+        bannerView = GADBannerView(adSize:kGADAdSizeBanner)
+        bannerView.frame.origin = CGPoint(x: 0, y: view.frame.height - bannerView.frame.height)
+        bannerView.frame.size = CGSize(width: self.view.frame.width, height: bannerView.frame.height)
+        bannerView.adUnitID = "\(YOUR_BARNER_ID)"
+        bannerView.delegate = self
+        bannerView.rootViewController = self
+        
+        let request:GADRequest = GADRequest()
+        
+        if AdMobTest {
+            if SimulatorTest {
+                request.testDevices = [kGADSimulatorID]
+            } else {
+                request.testDevices = [TEST_DEVICE_ID]
+            }
+        }
+        
+        bannerView.load(request)
+        
+        return bannerView
     }
 
 
